5c4b7c2011ac4e76c08ef8ffda02978225ac9310
rawuor <rawuor@u.rochester.edu>
2016-01-14 13:00:27 -0600

    update app.js

--- emailapp/js/app.js
+++ emailapp/js/app.js
@@ -1,1 +1,14 @@
 //js stuff
+angular.module ('EmailApp', ['ngRoute'].config(function, ()$routeProvider){
+	'use strict';
+	//Configure URLs
+	$routeProvider
+	.when('/inbox', {
+		templateUrl: 'views/inbox.html',
+		controller: 'InboxCtrl'
+	})
+	.otherwise({
+		//default
+		redirectTo: '/inbox'
+	});
+})
